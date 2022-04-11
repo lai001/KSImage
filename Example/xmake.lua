0a04e177c41396e5d30bc4875a9b31a0d56455b9
lai001 <1104698042@qq.com>
2022-04-11 21:07:15 +0800

    Update xmake.lua

--- Example/xmake.lua
+++ Example/xmake.lua
@@ -5,16 +5,23 @@ add_requires("spdlog")
 add_requires("stb")
 add_requires("glm")
 
-rule("ImGUI.Download")
-    on_load(function (target)
+task("deps")
+    on_run(function ()
+        import("devel.git")
+        if os.exists("../../Foundation") == false then
+            git.clone("https://github.com/lai001/Foundation.git", {branch = "main", outputdir = "../../Foundation"})
+        end
         if os.exists("./Vendor/imgui") == false then
-            import("devel.git")
             local oldir = os.cd("Vendor")
             git.clone("https://github.com/ocornut/imgui.git", {branch = "master", outputdir = "imgui"})
             git.checkout("v1.87", {repodir = "imgui"})
             os.cd(oldir)
         end
     end)
+    set_menu {
+        usage = "xmake deps",
+        description = "download deps"
+    }
 
 rule("Example.Copy")
     after_build(function (target)
@@ -59,7 +66,6 @@ target("Example")
 target("ImGui")
     set_kind("static")
     set_languages("c++17")
-    add_rules("ImGUI.Download")
     add_includedirs("Vendor/imgui")
     add_files("Vendor/imgui/*.cpp")
     add_files("Vendor/imgui/backends/imgui_impl_dx11.cpp")
